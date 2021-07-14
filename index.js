68694a25f36440af9b7d2dd05f022615c4be814f
Sawant_Siddhesh <43745879+siddhesh23sawant@users.noreply.github.com>
2021-07-14 22:43:14 +0530

    changes in js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
+// onLoad = alert(rnd_gen());
+
+
 function rnd_gen()
 {
     home = Math.random();
-    home = Math.floor(home * 1000);
+    home = Math.floor(home * 1000   );
     adopted = Math.random();
     adopted = Math.floor(adopted * 1200);
     vaccinated = Math.random();
     vaccinated = Math.floor(vaccinated * 500);
-    return home,adopted,vaccinated;
+    return  home; //,adopted,vaccinated
 }
 
-var home, adopted, vaccinated = rnd_gen();
+var home = rnd_gen(); //, adopted, vaccinated
 
 document.querySelector(".home").textContent = home;
-document.querySelector(".adopted").textContent = adopted;
-document.querySelector(".vaccinated").textContent = vaccinated;
-
+// document.querySelector(".adopted").textContent = adopted;
+// document.querySelector(".vaccinated").textContent = vaccinated;
 
 document.querySelector(".title_text").textContent = "Loved is a";
